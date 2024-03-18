e53d5342450466441a9efd4685ebc2b3b2162169
Larry Wen <l.wen@griffith.edu.au>
2024-03-18 17:13:26 +1000

    init project

---
+++ ex.test.js
@@ -0,0 +1,17 @@
+/**
+ * @file ex.test.js
+ * @description Test suite for Week 4 exercises of the Mobile Application Development
+ *              course. This file contains test cases that validate the correctness of
+ *              the code written in the exercises. The tests are designed to assess
+ *              the functionality of the implemented solutions against specified
+ *              requirements and scenarios.
+ * @author Larry Wen
+ * @created [18/03/2024]
+ *
+ * NOTE: This file is not intended to be modified by the students. Alterations to this
+ * test suite may result in inaccurate assessments of the exercise solutions.
+ */
+
+const {} = require("./questions");
+
+const { students, selectStudents } = require("./data");
